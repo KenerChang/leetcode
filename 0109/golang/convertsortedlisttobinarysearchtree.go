61dc5996cb534ad5a506e783c53a2d62e7a39c00
kenchang <workinkener@gmail.com>
2020-05-13 16:59:01 +0800

    improve performance by using map and virtual position index

--- 0109/golang/convertsortedlisttobinarysearchtree.go
+++ 0109/golang/convertsortedlisttobinarysearchtree.go
@@ -20,45 +20,36 @@ func sortedListToBST(head *ListNode) *TreeNode {
 		return nil
 	}
 
+	nodeMap := map[int]*ListNode{}
 	curNode := head
-	nodeCount := 0
+	idx := 0
 	for curNode != nil {
-		nodeCount++
+		nodeMap[idx] = curNode
+		idx++
 		curNode = curNode.Next
 	}
 
-	return sortedListToBSTImpl(head, nodeCount)
+	return sortedListToBSTImpl(nodeMap, 0, idx)
 }
 
-func sortedListToBSTImpl(start *ListNode, nodeCount int) *TreeNode {
-	if start == nil || nodeCount == 0 {
+func sortedListToBSTImpl(nodeMap map[int]*ListNode, start, end int) *TreeNode {
+	nodeCount := end - start
+	if nodeCount <= 0 {
 		return nil
 	}
 
 	if nodeCount == 1 {
+		node := nodeMap[start]
 		return &TreeNode{
-			Val: start.Val,
+			Val: node.Val,
 		}
 	}
 
-	idx := 0
-	middle := nodeCount / 2
-	curNode := start
-
-	var tree *TreeNode
-	for idx <= middle+1 && curNode != nil {
-		if idx == middle {
-			tree = &TreeNode{
-				Val:  curNode.Val,
-				Left: sortedListToBSTImpl(start, middle),
-			}
-		}
-
-		if idx == middle+1 {
-			tree.Right = sortedListToBSTImpl(curNode, nodeCount-middle-1)
-		}
-		idx++
-		curNode = curNode.Next
+	middle := (start + end) / 2
+	root := nodeMap[middle]
+	return &TreeNode{
+		Val:   root.Val,
+		Left:  sortedListToBSTImpl(nodeMap, start, middle),
+		Right: sortedListToBSTImpl(nodeMap, middle+1, end),
 	}
-	return tree
 }
