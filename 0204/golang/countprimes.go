4040ec2c936459b03d8aa4e60c3626994666ffed
kenchang <workinkener@gmail.com>
2020-07-21 20:55:20 +0800

    precompute multple of the prime

--- 0204/golang/countprimes.go
+++ 0204/golang/countprimes.go
@@ -1,7 +1,7 @@
 package countprimes
 
 import (
-	"math"
+// "math"
 )
 
 func countPrimes(n int) int {
@@ -9,26 +9,15 @@ func countPrimes(n int) int {
 		return 0
 	}
 
-	primes := []int{2}
-	for i := 3; i < n; i++ {
-		if !isPrime(i, primes) {
-			continue
-		}
-
-		primes = append(primes, i)
-	}
-	return len(primes)
-}
-
-func isPrime(n int, primes []int) bool {
-	sr := math.Sqrt(float64(n))
-	for _, prime := range primes {
-		if float64(prime) > sr {
-			break
-		}
-		if n%prime == 0 {
-			return false
+	isPrime := make([]bool, n)
+	count := 0
+	for i := 2; i < n; i++ {
+		if !isPrime[i] {
+			count++
+			for j := 2; i*j < n; j++ {
+				isPrime[i*j] = true
+			}
 		}
 	}
-	return true
+	return count
 }
