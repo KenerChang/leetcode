92427556e38f022717e958e43b6e88799d0ec7d5
kenchang <workinkener@gmail.com>
2020-07-23 21:53:36 +0800

    improve performance by using tree structure

--- 0208/golang/implementtrie.go
+++ 0208/golang/implementtrie.go
@@ -1,57 +1,68 @@
 package implementtrie
 
-import (
-	"regexp"
-)
-
 type Trie struct {
-	wordDict map[string]bool
-	wordList []string
-	a        int
+	children    [26]*Trie
+	isEndOfWord bool
+	root        *Trie
 }
-
-const a int = int('a')
 
 /** Initialize your data structure here. */
 func Constructor() Trie {
-	return Trie{
-		wordDict: map[string]bool{},
-		wordList: make([]string, 26),
-	}
+	r := Trie{children: [26]*Trie{}, isEndOfWord: false, root: &Trie{}}
+	return r
 }
 
 /** Inserts a word into the trie. */
 func (this *Trie) Insert(word string) {
-	if _, found := this.wordDict[word]; found {
+	if len(word) == 0 {
 		return
 	}
-
-	this.wordDict[word] = true
-
-	idx := int(word[0]) - a
-	this.wordList[idx] = this.wordList[idx] + word + ";"
+	t := this.root
+	for i := 0; i < len(word); i++ {
+		index := word[i] - 'a'
+		if t.children[index] == nil {
+			t.children[index] = &Trie{}
+		}
+		t = t.children[index]
+	}
+	t.isEndOfWord = true
 }
 
 /** Returns if the word is in the trie. */
 func (this *Trie) Search(word string) bool {
-	_, found := this.wordDict[word]
-	return found
+	t := this.root
+	for i := 0; i < len(word); i++ {
+		index := word[i] - 'a'
+		if t.children[index] == nil {
+			return false
+		}
+		t = t.children[index]
+	}
+	if t.isEndOfWord == true {
+		return true
+	}
+	return false
 }
 
 /** Returns if there is any word in the trie that starts with the given prefix. */
 func (this *Trie) StartsWith(prefix string) bool {
-	idx := int(prefix[0]) - a
-	charString := this.wordList[idx]
-
-	if len(charString) < len(prefix) {
-		return false
+	t := this.root
+	if len(prefix) == 1 {
+		if t.children[prefix[0]-'a'] != nil {
+			return true
+		}
 	}
-
-	if charString[0:len(prefix)] == prefix {
-		return true
+	depth := 0
+	for i := 0; i < len(prefix); i++ {
+		depth++
+		index := prefix[i] - 'a'
+		if t.children[index] == nil {
+			return false
+		}
+		if depth == len(prefix) {
+			return true
+		}
+		t = t.children[index]
 	}
-
-	pattern := ";" + prefix
-	match, _ := regexp.MatchString(pattern, charString)
-	return match
+	return false
 }
