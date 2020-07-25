417df928217f708932a740a61bea2a132292d4b9
kenchang <workinkener@gmail.com>
2020-07-25 23:28:34 +0800

    change map to slice to verify cycle

--- 0207/golang/courseschedule.go
+++ 0207/golang/courseschedule.go
@@ -10,34 +10,37 @@ func canFinish(numCourses int, prerequisites [][]int) bool {
 	}
 
 	adjacencyLists := make([][]int, numCourses)
+	visited := make([]int, numCourses)
 
 	for _, prerequisite := range prerequisites {
 		course, need := prerequisite[0], prerequisite[1]
 		adjacencyLists[course] = append(adjacencyLists[course], need)
 
-		if hasCycle(course, adjacencyLists, map[int]bool{}) {
+		if hasCycle(course, adjacencyLists, visited) {
 			return false
 		}
 	}
 	return true
 }
 
-func hasCycle(course int, adjacencyLists [][]int, visited map[int]bool) bool {
+func hasCycle(course int, adjacencyLists [][]int, visited []int) bool {
 	if len(adjacencyLists[course]) == 0 {
 		return false
 	}
 
-	if _, found := visited[course]; found {
+	if visited[course] == 2 {
 		return true
 	}
-	visited[course] = true
+
+	visited[course] = 2
 
 	adjacencyList := adjacencyLists[course]
 	for _, needCourse := range adjacencyList {
 		if hasCycle(needCourse, adjacencyLists, visited) {
+			visited[course] = 1
 			return true
 		}
-		delete(visited, needCourse)
+		visited[course] = 1
 	}
 	return false
 }
