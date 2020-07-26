839514c003c3101bb6c390501f33e0adf91e1d60
kenchang <workinkener@gmail.com>
2020-07-26 10:39:08 +0800

    improve performance by: 1. check cycle after adjacency lists has build 2. use slice of bool to keep visited & has visited in this cycle

--- 0210/golang/coursescheduleii.go
+++ 0210/golang/coursescheduleii.go
@@ -6,64 +6,46 @@ import (
 
 func findOrder(numCourses int, prerequisites [][]int) []int {
 	adjacencyLists := make([][]int, numCourses)
-	visited := make([]int, numCourses)
-
 	for _, prerequisite := range prerequisites {
 		course, need := prerequisite[0], prerequisite[1]
 
 		adjacencyLists[course] = append(adjacencyLists[course], need)
-		if hasCycle(course, adjacencyLists, visited) {
+	}
+
+	order := []int{}
+	var cycle bool
+	visited := make([]bool, numCourses)
+	for course := 0; course < numCourses; course++ {
+		cycleVisited := make([]bool, numCourses)
+		cycle, order = trace(course, adjacencyLists, cycleVisited, visited, order)
+		if cycle {
 			return []int{}
 		}
 	}
 
-	order := []int{}
-	for course := 0; course < numCourses; course++ {
-		order = trace(course, adjacencyLists, visited, order)
-	}
-
 	return order
 }
 
-func hasCycle(course int, adjacencyLists [][]int, visited []int) bool {
-	if len(adjacencyLists[course]) == 0 {
-		return false
+func trace(course int, adjacencyLists [][]int, thisCycleVisited, visited []bool, order []int) (bool, []int) {
+	if thisCycleVisited[course] {
+		return true, []int{}
 	}
 
-	if visited[course] == 2 {
-		return true
-	}
-
-	visited[course] = 2
-
-	adjacencyList := adjacencyLists[course]
-	for _, needCourse := range adjacencyList {
-		if hasCycle(needCourse, adjacencyLists, visited) {
-			visited[course] = 1
-			return true
-		}
-		visited[course] = 1
-	}
-	return false
-}
-
-func trace(course int, adjacencyLists [][]int, visited, order []int) []int {
-	if visited[course] == 2 {
-		return order
-	}
-
-	if len(adjacencyLists[course]) == 0 {
-		order = append(order, course)
-		visited[course] = 2
-		return order
+	if visited[course] {
+		return false, order
 	}
 
 	needs := adjacencyLists[course]
+	var cycle bool
+	thisCycleVisited[course] = true
 	for _, needCourse := range needs {
-		order = trace(needCourse, adjacencyLists, visited, order)
+		cycle, order = trace(needCourse, adjacencyLists, thisCycleVisited, visited, order)
+		if cycle {
+			return true, order
+		}
 	}
-
-	visited[course] = 2
+	thisCycleVisited[course] = false
+	visited[course] = true
 	order = append(order, course)
-	return order
+	return false, order
 }
