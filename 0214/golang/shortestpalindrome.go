6a833758af694e2e828fb99ca721d19558c40975
kenchang <workinkener@gmail.com>
2020-08-01 22:29:13 +0800

    improve performance by genius solution of WinstonHanxb (https://leetcode.com/problems/shortest-palindrome/discuss/166018/go-two-pointers-solution)

--- 0214/golang/shortestpalindrome.go
+++ 0214/golang/shortestpalindrome.go
@@ -1,45 +1,36 @@
 package shortestpalindrome
 
+import (
+// "fmt"
+)
+
 func shortestPalindrome(s string) string {
 	if len(s) <= 1 {
 		return s
 	}
 
-	bytes := []byte(s)
-	var left, right int
-	var reset bool = true
-	for left >= 0 || right < len(bytes) {
-		if reset {
-			even := (len(bytes) % 2) == 0
-			middle := len(bytes) / 2
-			if even {
-				right = middle
-				left = right - 1
-			} else {
-				right = middle + 1
-				left = middle - 1
-			}
-			reset = false
-		} else {
-			if bytes[left] == '#' {
-				bytes[left] = bytes[right]
-				left--
-				right++
-			} else if bytes[left] == bytes[right] {
-				left--
-				right++
-			} else {
-				reset = true
-				bytes = append([]byte{'#'}, bytes...)
-			}
-		}
-	}
+	i, j := 0, len(s)-1
 
-	i, j := 0, len(bytes)-1
-	for i < j {
-		bytes[i] = bytes[j]
-		i++
+	// find max common match
+	for j >= 0 {
+		if s[i] == s[j] {
+			i++
+		}
 		j--
 	}
-	return string(bytes)
+
+	if i == len(s) {
+		return s
+	}
+
+	// shortest palidrome is composed of reverse of non match part + shortestPalindrome of match part + non match part
+	return reverse(s[i:]) + shortestPalindrome(s[:i]) + s[i:]
+}
+
+func reverse(s string) string {
+	runes := []rune(s)
+	for from, to := 0, len(runes)-1; from < to; from, to = from+1, to-1 {
+		runes[from], runes[to] = runes[to], runes[from]
+	}
+	return string(runes)
 }
