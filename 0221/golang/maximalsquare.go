122faa65a0cef80b4778f459f4946ea733abb4bd
kenchang <workinkener@gmail.com>
2021-04-02 14:10:01 +0800

    use dp

--- 0221/golang/maximalsquare.go
+++ 0221/golang/maximalsquare.go
@@ -1,26 +1,27 @@
 package maximalsquare
 
+import "math"
+
 func maximalSquare(matrix [][]byte) int {
 	// one := '1'
 	maxSquare := 0
-	for i := 0; i < len(matrix); i++ {
-		for j := 0; j < len(matrix[0]); j++ {
-			if matrix[i][j] == '1' {
 
-				if i+maxSquare-1 >= len(matrix) || j+maxSquare-1 >= len(matrix[0]) {
-					continue
-				}
+	// allocate dp
+	m := len(matrix)
+	n := len(matrix[0])
+	dp := make([][]int, m+1)
+	for i := 0; i <= m; i++ {
+		dp[i] = make([]int, n+1)
+	}
 
-				size := 1
-				for {
-					result := isSquare(matrix, i, j, size+1)
-					if !result {
-						if size > maxSquare {
-							maxSquare = size
-						}
-						break
-					}
-					size++
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if matrix[i-1][j-1] == '1' {
+				size := 1 + min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])
+				dp[i][j] = size
+
+				if size > maxSquare {
+					maxSquare = size
 				}
 			}
 		}
@@ -29,21 +30,12 @@ func maximalSquare(matrix [][]byte) int {
 	return maxSquare * maxSquare
 }
 
-func isSquare(matrix [][]byte, x, y, size int) bool {
-	if x+(size-1) >= len(matrix) {
-		return false
-	}
-
-	if y+(size-1) >= len(matrix[0]) {
-		return false
-	}
-
-	for i := 0; i < size; i++ {
-		for j := 0; j < size; j++ {
-			if matrix[x+i][y+j] == '0' {
-				return false
-			}
+func min(nums ...int) int {
+	result := math.MaxInt32
+	for _, num := range nums {
+		if num < result {
+			result = num
 		}
 	}
-	return true
+	return result
 }
