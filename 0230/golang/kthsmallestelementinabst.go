e93b86dc47394dca06b179f0fd125e2491e9a81f
kenchang <workinkener@gmail.com>
2021-04-08 21:38:54 +0800

    improve performance by using count variable

--- 0230/golang/kthsmallestelementinabst.go
+++ 0230/golang/kthsmallestelementinabst.go
@@ -7,17 +7,22 @@ type TreeNode struct {
 }
 
 func kthSmallest(root *TreeNode, k int) int {
-	results := kthSmallestRecursive(root, k, []int{})
-	return results[k-1]
+	_, result := kthSmallestRecursive(root, k, -1)
+	return result
 }
 
-func kthSmallestRecursive(root *TreeNode, k int, track []int) []int {
-	if len(track) == k || root == nil {
-		return track
+func kthSmallestRecursive(root *TreeNode, remain, result int) (int, int) {
+	if remain == 0 || root == nil {
+		return remain, result
 	}
 
-	track = kthSmallestRecursive(root.Left, k, track)
-	track = append(track, root.Val)
-	track = kthSmallestRecursive(root.Right, k, track)
-	return track
+	remain, result = kthSmallestRecursive(root.Left, remain, result)
+	if remain <= 0 {
+		return remain, result
+	}
+
+	if remain == 1 {
+		return remain - 1, root.Val
+	}
+	return kthSmallestRecursive(root.Right, remain-1, result)
 }
