29c84df265a1fef9642162300b02999e7468e20c
kenchang <workinkener@gmail.com>
2022-09-03 20:25:56 +0800

    one pass solution

--- 0299/golang/bullsandcows.go
+++ 0299/golang/bullsandcows.go
@@ -2,37 +2,31 @@ package bullsandcows
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func getHint(secret string, guess string) string {
 	// use a map to store the count of each digit
 	digitMap := map[byte]int{}
-	for _, digit := range secret {
-		digitMap[byte(digit)]++
-	}
-
-	// count how many a
 	aCount := 0
-	usedMap := map[string]bool{}
-	for i, digit := range guess {
-		if secret[i] == guess[i] {
-			digitMap[byte(digit)]--
-			aCount++
-
-			key := string(digit) + strconv.Itoa(i)
-			usedMap[key] = true
-		}
-	}
-
-	// count how many b
 	bCount := 0
-	for i, digit := range guess {
-		key := string(digit) + strconv.Itoa(i)
-		_, ok := usedMap[key]
-		if !ok && digitMap[byte(digit)] > 0 {
-			digitMap[byte(digit)]--
-			bCount++
+	for i := range secret {
+		s := secret[i]
+		g := guess[i]
+		if s == g {
+			aCount++
+		} else {
+			// if s is in guess, then digitMap[s] must < 0
+			if digitMap[s] < 0 {
+				bCount++
+			}
+
+			// i g is in secret, then digitMap[g] must > 0
+			if digitMap[g] > 0 {
+				bCount++
+			}
+
+			digitMap[s]++
+			digitMap[g]--
 		}
 	}
 
