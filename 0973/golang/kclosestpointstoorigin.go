63523aac9467bdf3cb44c714ca5772aa43cc7240
kenchang <workinkener@gmail.com>
2022-09-12 22:35:46 +0800

    leetcode 0973 use quick select

--- 0973/golang/kclosestpointstoorigin.go
+++ 0973/golang/kclosestpointstoorigin.go
@@ -1,58 +1,49 @@
 package kclosestpointstoorigin
 
-import (
-	"container/heap"
-)
+type Point []int
 
-type Points []Point
-
-func (h Points) Len() int { return len(h) }
-func (h Points) Less(i, j int) bool {
-	pointI := h[i]
-	pointJ := h[j]
-
-	return (pointI.X*pointI.X + pointI.Y*pointI.Y) < (pointJ.X*pointJ.X + pointJ.Y*pointJ.Y)
-}
-func (h Points) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
-
-func (h *Points) Push(x any) {
-	// Push and Pop use pointer receivers because they modify the slice's length,
-	// not just its contents.
-	*h = append(*h, x.(Point))
-}
-
-func (h *Points) Pop() any {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
-}
-
-type Point struct {
-	X int
-	Y int
+func (p Point) distance() int {
+	return p[0]*p[0] + p[1]*p[1]
 }
 
 func kClosest(points [][]int, k int) [][]int {
-	// we can use heap to solve this problem
-	// and the time complexity is O(klogn) + O(n)
-
-	// init heap
-	h := &Points{}
-	heap.Init(h)
-	for _, point := range points {
-		p := Point{X: point[0], Y: point[1]}
-		heap.Push(h, p)
+	// since we are finding the top k closest points, we can use quick select
+	if len(points) == k {
+		return points
 	}
 
-	// pop k elements
-	results := [][]int{}
-	for i := 0; i < k; i++ {
-		p := heap.Pop(h).(Point)
+	quickSelect(points, 0, len(points)-1, k)
 
-		results = append(results, []int{p.X, p.Y})
+	return points[:k]
+}
+
+func quickSelect(ps [][]int, l, r, k int) {
+	if l-r == 1 {
+		return
 	}
 
-	return results
+	// choose last point as pivot
+	pivotDistance := Point(ps[r]).distance()
+	i := l
+	for j := l; j < r; j++ {
+		if Point(ps[j]).distance() <= pivotDistance {
+			ps[i], ps[j] = ps[j], ps[i]
+			i++
+		}
+	}
+
+	// swap pivot
+	ps[i], ps[r] = ps[r], ps[i]
+
+	if i-l == k-1 {
+		return
+	}
+
+	if i-l > k-1 {
+		// search left
+		quickSelect(ps, l, i-1, k)
+	} else {
+		// search right
+		quickSelect(ps, i+1, r, k-i+l-1)
+	}
 }
