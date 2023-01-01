4e1e4af2bfe50ad5671ab49a28e8be0c380a6107
kenchang <workinkener@gmail.com>
2023-01-01 16:43:35 +0800

    solve in O(n)

--- 0503/golang/nextgreaterelementii.go
+++ 0503/golang/nextgreaterelementii.go
@@ -1,43 +1,19 @@
 package nextgreaterelementii
 
 func nextGreaterElements(nums []int) []int {
-	// find the left most greater value for each num
-	// find the next greter value for each num
-	// if a num has next greater value => next greater value
-	// else if a num has left most greater value => left most greater value
-	// else -1
+	// we could iterate the nums twice, for a number num, the numbers appear before num
+	// can be viewed as apperaing after num, it equals to find next greater element of num
 
-	leftMostGreaters := make([]int, len(nums))
-	leftMostGreaters[0] = -1
-
-	queue := []int{nums[0]} // montonic ascending queue
-	for i, num := range nums {
-		if num > queue[len(queue)-1] {
-			queue = append(queue, num)
-			leftMostGreaters[i] = -1
-		} else if num < queue[0] {
-			leftMostGreaters[i] = queue[0]
-		} else {
-			var found bool
-			for j := 0; j < len(queue); j++ {
-				if queue[j] > num {
-					leftMostGreaters[i] = queue[j]
-					found = true
-					break
-				}
-
-				if !found {
-					leftMostGreaters[i] = -1
-				}
-			}
-		}
+	stack := [][]int{}
+	results := make([]int, len(nums))
+	for i := range nums {
+		results[i] = -1
 	}
 
-	// fmt.Printf("leftMostGreaters: %v\n", leftMostGreaters)
+	for i := 0; i < 2*len(nums); i++ {
+		pos := i % len(nums)
+		num := nums[pos]
 
-	stack := [][]int{} // 0: num index 1: val // monotonic ascending
-	results := make([]int, len(nums))
-	for i, num := range nums {
 		for len(stack) > 0 && num > stack[len(stack)-1][1] {
 			top := stack[len(stack)-1]
 			stack = stack[0 : len(stack)-1]
@@ -45,15 +21,7 @@ func nextGreaterElements(nums []int) []int {
 			results[top[0]] = num
 		}
 
-		stack = append(stack, []int{i, num})
+		stack = append(stack, []int{pos, num})
 	}
-
-	for len(stack) > 0 {
-		top := stack[len(stack)-1]
-		stack = stack[0 : len(stack)-1]
-
-		results[top[0]] = leftMostGreaters[top[0]]
-	}
-
 	return results
 }
